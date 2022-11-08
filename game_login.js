7373ecce13796bdbfc4ad4431fe246ecd7377f99
Arav1105 <111154791+Arav1105@users.noreply.github.com>
2022-11-07 19:37:43 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,33 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+    function Set()
+    localStorage.setItem("player1_name");
+    function Set()
+    localStorage.setItem("player2_name");
+    window.location = "game_page.html";
+}
+
+// Create function addUser()
+function addUser {
+  // Get value of user by id player1_name_input and player2_name_input
+var 
+  // Store these values locally
+
+  //Assign "game_page.html" to window.location
+  
+}
+function send()
+{
+Number1 = document.getElementById("number1").value
+Number2=  document.getElementById("number2").value
+actual_answer = parseInt(Number1) * parseInt(Number2);
+
+question_number = "<h4>" + Number1 + "X"+ number2 +"</h4>";
+input_box = "<br> Answer : <input type = 'text' id ='input_check_box'>";
+check_box = "<br><br><button class ='btn btn-info' onclick ='check()'> check </button>"
+}
